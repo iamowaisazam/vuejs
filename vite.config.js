23ec9a65eea57a7377f4be343a37213b842c107f
iamowaisazam <iamowaisazam@gmail.com>
2023-06-04 20:23:28 +0500

    up

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/vuejs my-first-project/',
+  publicPath: '/vuejs/',
   plugins: [vue()],
   resolve: {
     alias: {
